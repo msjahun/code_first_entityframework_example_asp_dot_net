791fd8fa12fdd4b628b6fe71f845eca0746c0abc
Musa Jahun <msjahun@live.com>
2018-07-12 22:29:46 +0300

    added BlogginContext class which inherits DbContext

--- Code_first_entity_framework_example_asp_dot_net/Program.cs
+++ Code_first_entity_framework_example_asp_dot_net/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Code_first_entity_framework_example_asp_dot_net
 {
@@ -29,5 +30,11 @@ namespace Code_first_entity_framework_example_asp_dot_net
 
         public int BlogId { get; set; }
         public virtual Blog Blog { get; set; }
+    }
+
+    public class BlogginContext : DbContext
+    {
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Post> Posts { get; set; }
     }
 }
