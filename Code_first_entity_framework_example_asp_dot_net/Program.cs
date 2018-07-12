5d198a523649ede97989d6644a65fa54c2d13da7
Musa Jahun <msjahun@live.com>
2018-07-12 22:21:54 +0300

    Add project files.

---
+++ Code_first_entity_framework_example_asp_dot_net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_first_entity_framework_example_asp_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
