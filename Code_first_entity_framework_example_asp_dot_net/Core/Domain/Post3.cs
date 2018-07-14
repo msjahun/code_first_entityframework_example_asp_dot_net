45e9f7fdc01c572ee79982e510d9f36581edcaaa
Musa Jahun <msjahun@live.com>
2018-07-14 16:24:16 +0300

    Refacted Post3 class  from Program.cs to it's own separate class

---
+++ Code_first_entity_framework_example_asp_dot_net/Core/Domain/Post3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_first_entity_framework_example_asp_dot_net.Core.Domain
+{
+    public class Post3
+    {/// <summary>
+     /// 
+     /// </summary>
+        public int Post3Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
