1dff0fec217f5a4389a1129fa50a1e34bfe88cc1
Musa Jahun <msjahun@live.com>
2018-07-14 16:24:47 +0300

    Refacted BlogginContext to Persistence folder

---
+++ Code_first_entity_framework_example_asp_dot_net/Persistence/BlogginContext.cs
@@ -0,0 +1,21 @@
+﻿using Code_first_entity_framework_example_asp_dot_net.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_first_entity_framework_example_asp_dot_net.Persistence
+{
+
+    public class BlogginContext : DbContext
+    {/// <summary>
+     /// 
+     /// </summary>
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Post2> Posts2 { get; set; }
+        public DbSet<Post3> Posts3 { get; set; }
+    }
+}
