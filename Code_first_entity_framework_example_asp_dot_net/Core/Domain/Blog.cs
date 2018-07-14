a231b2796579648eda2ff47c77897c8e9d8a0229
Musa Jahun <msjahun@live.com>
2018-07-14 16:23:38 +0300

    Refacted Blog class  from Program.cs to it's own separate class

---
+++ Code_first_entity_framework_example_asp_dot_net/Core/Domain/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_first_entity_framework_example_asp_dot_net.Core.Domain
+{
+    public class Blog
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+        public virtual List<Post2> Posts2 { get; set; }
+
+        public virtual List<Post3> Posts3 { get; set; }
+    }
+}
